2dc08862fadf217ff984b8ebebc7b231433b4db4
kura84git <kou@kura84.com>
2020-04-27 22:44:27 +0900

    add
    add

--- src/test/test.java
+++ src/test/test.java
@@ -4,11 +4,13 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
+		System.out.println("こんばんは");
 		System.out.println("Hello");
 		System.out.println("second commit");
 		System.out.println("third commit");
 		System.out.println("fourth commit");
-		// TODO 自動生成されたメソッド・スタブ
+
+
 
 	}
 
