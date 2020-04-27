14fc5e81a7cb038746e96256cfefce6b13fe0eae
kura84git <kou@kura84.com>
2020-04-27 22:45:34 +0900

    test commit
    test commit

--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
 		System.out.println("こんばんは");
+		System.out.println("さようなら");
 		System.out.println("Hello");
 		System.out.println("second commit");
 		System.out.println("third commit");
